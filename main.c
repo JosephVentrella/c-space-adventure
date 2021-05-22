ff7b43a3aa21cef65939ba9baec7a17ae1ec4eb3
JosephVentrella <ventrejo@oregonstate.edu>
2021-05-21 23:40:34 -0700

    Create getName function and extract relevant statements to it.

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+char name[25];
 void displayGreeting(){
 	printf("Welcome to the Solar System!\n");
 	printf("There are 9 planets to explore.\n");
 	printf("What is your name traveler?\n");
 }
-int main(int argc, char** argv){
-	displayGreeting();
-	char name[25];
+void readName(){
 	fgets(name, 25, stdin);
 	name[strcspn(name, "\n")] = 0;
+}
+int main(int argc, char** argv){
+	displayGreeting();
+	readName();
 	printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
 	printf("Should I randomly choose a planet for you to visit? (Y or N)\n");
 	char randomPlanet[2];
