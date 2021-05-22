28addbd8f48cebb60f6be7d102f68e739399ade6
JosephVentrella <ventrejo@oregonstate.edu>
2021-05-21 20:28:47 -0700

    Prompt user and read input for name and travel destination.

--- main.c
+++ main.c
@@ -3,5 +3,14 @@
 int main(int argc, char** argv){
 	printf("Welcome to the Solar System!\n");
 	printf("There are 9 planets to explore.\n");
+	printf("What is your name traveler?\n");
+	char name[25];
+	fgets(name, 25, stdin);
+	printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
+	printf("Should I randomly choose a planet for you to visit? (Y or N)\n");
+	char randomPlanet[2];
+	fgets(randomPlanet, 25, stdin);
+	printf("Traveling to Jupiter...\n");
+	printf("Arrived at Jupiter, look at those rings.\n");
 	return 0;
 }
