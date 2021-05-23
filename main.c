1d3204740e917b7536697b5d4f4a3f3e69695cfd
JosephVentrella <ventrejo@oregonstate.edu>
2021-05-22 21:01:21 -0700

    Rename variable holding random planet response, and extract random planet choice to a function.

--- main.c
+++ main.c
@@ -14,14 +14,18 @@ void readName(){
 	fgets(name, 25, stdin);
 	name[strcspn(name, "\n")] = 0;
 }
+
+void chooseRandomPlanet(){
+	char yesOrNo[2];
+	fgets(yesOrNo, 25, stdin);
+	yesOrNo[strcspn(yesOrNo, "\n")]=0;
+	printf("You picked %s\n", yesOrNo);
+}
 int main(int argc, char** argv){
 	displayGreeting();
 	readName();
 	greetingResponse();
-	char randomPlanet[2];
-	fgets(randomPlanet, 25, stdin);
-	randomPlanet[strcspn(randomPlanet, "\n")]=0;
-	printf("You picked %s\n", randomPlanet);
+	chooseRandomPlanet();
 	printf("Traveling to Jupiter...\n");
 	printf("Arrived at Jupiter, look at those rings.\n");
 	return 0;
