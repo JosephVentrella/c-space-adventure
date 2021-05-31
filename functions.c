6d5e4cb92ebe48e84ddf36e1846ccc8dc3ea8257
JosephVentrella <ventrejo@oregonstate.edu>
2021-05-31 00:10:45 -0700

    Create a travel to random planet function and implement a switch statement.

--- functions.c
+++ functions.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "functions.h"
+#include <stdlib.h>
+#include <time.h>
 char name[25];
 void displayGreeting(){
 	printf("Welcome to the Solar System!\n");
@@ -35,5 +37,55 @@ void choosePlanet(){
 	else {
 		chooseOwnPlanet();
 		travelToPlanet();
+	}
+}
+void travelToRandomPlanet(){
+	int planet = rand() % 10;
+	switch(planet){
+		case 1:
+			printf("Traveling to Mercury.\n");
+			printf("Arrived at Mercury.\n");
+			printf("Look how big the sun looks from here.\n");
+			break;
+		case 2:
+			printf("Traveling to Venus.\n");
+			printf("Arrived at Venus.\n");
+			printf("Its hard to breathe with all the clouds here.\n");
+			break;
+		case 3:
+			printf("Traveling to Earth.\n");
+			printf("Arrived at Earth.\n");
+			printf("This place looks familiar.\n");
+			break;
+		case 4:
+			printf("Traveling to Mars.\n");
+			printf("Arrived at Mars.\n");
+			break;
+		case 5:
+			printf("Traveling to Jupiter...\n");
+			printf("Arrived at Jupiter, look at those rings.\n");
+			break;
+		case 6:
+			printf("Traveling to Saturn.\n");
+			printf("Arrived at Saturn.\n");
+			printf("I dont know anything about this planet.\n");
+			break;
+		case 7:
+			printf("Traveling to Uranus.\n");
+			printf("Arrived at Uranus.\n");
+			printf("No jokes please.\n");
+			break;
+		case 8:
+			printf("Traveling to Neptune.\n");
+			printf("Arrived at Neptune.\n");
+			printf("Brrrrr its cold.\n");
+			break;
+		case 9:
+			printf("Traveling to Pluto.\n");
+			printf("Arrived at Pluto.\n");
+			printf("I dont care what they say it is definitely a planet.\n");
+			break;
+
+
 	}
 }
